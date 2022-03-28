2cf9645f3c2b3e71e5a160345f140ddb4f4b3b44
hugosenari <hugosenari@gmail.com>
2022-03-27 21:56:16 -0300

    chore: re index sigv4

--- 1_0_12/flake.nix
+++ 1_0_12/flake.nix
@@ -13,6 +13,22 @@
   inputs.src-sigv4-1_0_12.repo  = "sigv4";
   inputs.src-sigv4-1_0_12.type  = "github";
   
+  inputs."nimsha2".owner = "nim-nix-pkgs";
+  inputs."nimsha2".ref   = "master";
+  inputs."nimsha2".repo  = "nimsha2";
+  inputs."nimsha2".dir   = "master";
+  inputs."nimsha2".type  = "github";
+  inputs."nimsha2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimsha2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."github.com/disruptek/testes".owner = "nim-nix-pkgs";
+  inputs."github.com/disruptek/testes".ref   = "master";
+  inputs."github.com/disruptek/testes".repo  = "github.com/disruptek/testes";
+  inputs."github.com/disruptek/testes".dir   = "";
+  inputs."github.com/disruptek/testes".type  = "github";
+  inputs."github.com/disruptek/testes".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."github.com/disruptek/testes".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
